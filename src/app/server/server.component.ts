62ee8f783966e5bc7df86bc41fae830bf1308744
unknown <malinduruwantha@gmail.com>
2019-01-24 12:48:23 +0530

    second tutorial

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,22 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
 @Component({
     selector:'app-server',
     templateUrl:'./server.component.html'
 })
-export class Servercomponent{
+export class Servercomponent implements OnInit {
 Name = 'malindu Ruwantha';
 Id = 10;
 numberone=0;
 numbertwo=0;
 total=0;
+allowNewServer=false;
+innerhtmlTotal=0;
+
+
 getName(){
     return this.Name;
 }
 
 getvalue(){
     this.total=this.numberone+this.numbertwo;
+    this.innerhtmlTotal=this.total;
     return this.total;
+    
+}
+    constructor() { 
+        setTimeout( ()=>{
+            this.allowNewServer=true;
+        },2000)
+    }
+ 
+    ngOnInit() {
+    }
 }
 
 
-}
